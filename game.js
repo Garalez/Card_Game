da1c7be1ed2fef638863b7088aa27decaf889414
Vlad Galcev <Vladgalcev03@gmail.com>
2022-06-03 20:21:22 +0300

    Функционал доделал для сингл плеера

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
 /* eslint-disable max-len */
 'use strict';
 const questions = document.querySelector('.task__text');
+const gameTitle = document.querySelector('.game__title');
 const cardsItems = document.querySelectorAll('.game__items');
-// const cardsList = document.querySelectorAll('.game__list');
+const cardsList = document.querySelector('.game__list');
 const cardText = document.querySelectorAll('.game__items-text');
-const playerChoose = document.querySelectorAll('.answer__item');
+// const playerChoose = document.querySelectorAll('.answer__item');
 const playerChooseGameCard = document.querySelector('.answer__list');
 const button = document.querySelector('.next-round');
 const questionsArr = 'Когда я, создавал свою, первую многопользовательскую игру, я написал код так, что игрок мог стрелять тогда, когда отправлялос, сообщение о стрельбе, которое со стороны, клиента было связано с, нажатием кнопки мыши, Умелый игрок мог, воспользоваться этим, вставив строчку на JavaScript, очень похожую на ту, что упоминалась выше, чтобы заполучить почти, неограниченную скорость стрельбы.';
 const str = 'Когда я, создавал свою, первую многопользовательскую игру, я написал код так, что игрок мог стрелять тогда, когда отправлялос, сообщение о стрельбе, которое со стороны, клиента было связано с, нажатием кнопки мыши, Умелый игрок мог, воспользоваться этим, вставив строчку на JavaScript, очень похожую на ту, что упоминалась выше, чтобы заполучить почти, неограниченную скорость стрельбы.';
-const rndDrop = Math.floor(Math.random() * 5);
+// const rndDrop = Math.floor(Math.random() * 5);
 const goodToGo = str.split(' ').sort(() => 0.5 - Math.random());
 const questionsToGo = questionsArr.split(' ').sort(() => 0.5 - Math.random());
 const createCard = document.createElement('li');
 const createText = document.createElement('div');
-const text = document.createTextNode(goodToGo[10]);
 createText.classList.add('game__items-text');
 createCard.classList.add('game__items');
-createText.append(text);
 
 cardText.forEach((n, i) => {
   n.textContent = goodToGo[i];
@@ -29,50 +28,40 @@ const iterator = array => {
     const value = array[index];
     if (index < array.length) {
       index++;
+    } else {
+      alert('Карты или вопросы закончились)');
     }
     return value;
   };
 };
-const questionsItems = iterator(questionsToGo);
+const questionsText = iterator(questionsToGo);
+const answersText = iterator(goodToGo);
 
-cardsItems[0].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[0]);
-});
-cardsItems[1].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[1]);
-});
-cardsItems[2].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[2]);
-});
-cardsItems[3].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[3]);
-});
-cardsItems[4].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[4]);
-});
-cardsItems[5].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[5]);
-});
-cardsItems[6].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[6]);
-});
-cardsItems[7].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[7]);
-});
-cardsItems[8].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[8]);
-});
-cardsItems[9].addEventListener('click', () => {
-  playerChoose[rndDrop].replaceWith(cardsItems[9]);
+cardsList.addEventListener('click', e => {
+  const target = e.target;
+  if (target.closest('.game__items')) {
+    playerChooseGameCard.append(target.closest('.game__items'));
+    cardsList.classList.add('disabled-game_list');
+    gameTitle.style.display = 'none';
+  }
 });
 
 const swapCards = () => {
-  cardsItems[9].after(createCard);
-  createCard.append(createText);
-  while (playerChooseGameCard.firstChild) {
+  createText.textContent = answersText();
+  const createli = document.createElement('li');
+  const createDiv = document.createElement('div');
+  cardsList.append(createli);
+  createli.append(createDiv);
+  createDiv.append(createText.textContent);
+  createDiv.classList.add('game__items-text');
+  createli.classList.add('game__items');
+
+  while (playerChooseGameCard.firstChild) { // Удаление карт использованных
     playerChooseGameCard.removeChild(playerChooseGameCard.firstChild);
   }
-  questions.replaceWith(questionsItems());
+  questions.textContent = questionsText(); // изменение вопроса по клику
+  cardsList.classList.remove('disabled-game_list');
+  gameTitle.style.display = 'block';
 };
 
 
